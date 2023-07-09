08455eccca64d52ec88ca4958a96687c880898b3
OlgaNovik1 <7755517@gmail.com>
2023-07-09 22:50:58 +0300

    favorites

--- src/components/RepoCard.tsx
+++ src/components/RepoCard.tsx
@@ -1,5 +1,7 @@
-import React, { FC } from 'react'
+import React, { FC, useState } from 'react'
 import { IRepo } from '../models/models'
+import { useActions } from '../hooks/actions'
+import { useAppSelector } from '../hooks/redux'
 
 interface RepoCardProps {
     repo: IRepo
@@ -7,6 +9,24 @@ interface RepoCardProps {
 
 
 const RepoCard: FC<RepoCardProps> = ({ repo }) => {
+    const { favorites } = useAppSelector(state => state.githubReducer)
+    const { addToFavorites, deleteInFavorites } = useActions();
+
+    const [isFavorites, setIsFavorites] = useState(favorites.includes(repo.html_url))
+
+    const addToFaforite = (e: React.MouseEvent<HTMLButtonElement>) => {
+        e.preventDefault();
+        addToFavorites(repo.html_url)
+        setIsFavorites(true)
+    }
+
+    const deleteFavorite = (e: React.MouseEvent<HTMLButtonElement>) => {
+        e.preventDefault();
+        deleteInFavorites(repo.html_url)
+        setIsFavorites(false)
+
+    }
+
     return (
         <div className='border py-3 px-5 rounded mb-2 hover:shadow-md hover:bg-gray-100 transition-all'>
             <a href={repo.html_url} target='_blank'>
@@ -16,6 +36,16 @@ const RepoCard: FC<RepoCardProps> = ({ repo }) => {
                     Watchers: <span className='font-bold'>{repo.watchers}</span>
                 </p>
                 <p className='text-sm font-thin'>{repo?.description}</p>
+                {!isFavorites && <button
+                    onClick={(e) => addToFaforite(e)}
+                    className='py-2 mr-2 px-4 mt-2 bg-yellow-400 rounded hover:shadow-md transition-all'>
+                    ADD TO FAVORITES
+                </button>}
+                {isFavorites && <button
+                    onClick={(e) => deleteFavorite(e)}
+                    className='py-2 px-4 mt-2 bg-teal-400 rounded hover:shadow-md transition-all'>
+                    DELETE
+                </button>}
             </a>
 
         </div>
--- src/pages/FavoritesPage.tsx
+++ src/pages/FavoritesPage.tsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { useAppSelector } from '../hooks/redux'
 
 const FavoritesPage = () => {
+    const { favorites } = useAppSelector(state => state.githubReducer);
+
+    if (favorites.length === 0) return <p className='text-center text-black-600 mt-[85px]'>Репозитории отсутствуют...</p>
     return (
-        <div>Favorites</div>
+        <div className='flex justify-center pt-10 mx-auto h-screen w-screen'>
+            <ul className='list-none'>
+                {favorites.map(url => (
+                    <li key={url}>
+                        <a href={url} target="_blank">{url}</a>
+                    </li>
+                ))}
+            </ul>
+        </div>
     )
 }
 
